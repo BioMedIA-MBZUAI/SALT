9a1c844ec5efcce8d4d20261e7347dd0266eac61
Abdelrahman El Sayed (python) <53436138+python-arch@users.noreply.github.com>
2025-03-17 06:46:22 +0200

    Delete AllinonSAM directory

--- AllinonSAM/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --job-name=lora_without_decoder
-#SBATCH --gres gpu:0
-#SBATCH --nodes 1
-#SBATCH --cpus-per-task=12
-
-nvidia-smi
-
-python /home/abdelrahman.elsayed/med-cvpr/AllinonSAM/main.py
